d62489d3412627736665266367dcc9978e18ee18
Mathieu Malette <matt_malette_666@hotmail.com>
2018-02-22 11:35:02 -0500

    ajout de la fonction vider

--- 01_app.js
+++ 01_app.js
@@ -3,10 +3,11 @@ const app = express()
 const fs = require('fs')
 const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
+const ObjectID = require('mongodb').ObjectID;
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
-const ObjectID = require('mongodb').ObjectID;
+
 
 app.get('/', (req, res) => {
   res.render('gabarit.ejs')  
@@ -18,10 +19,11 @@ app.get('/adresses', (req, res) => {
  	console.log(JSON.stringify(resultat))
  	// transfert du contenu vers la vue index.ejs (renders)
  	// affiche le contenu de la BD          
- 	res.render('gabaritAdresses.ejs', {adresses: resultat})  
+ 	res.render('gabaritAdresses.ejs', {adresse: resultat})  
   })
 })
 
+//application ajouter
 app.post('/ajouter', (req, res) => {
 	db.collection('adresse').save(req.body, (err, result) => {
  		if (err) return console.log(err)
@@ -30,15 +32,37 @@ app.post('/ajouter', (req, res) => {
  	})
 })
 
+//application détruire
 app.get('/detruire/:id', (req, res) => {
 
  db.collection('adresse')
- .findOneAndDelete( {'_id': req.params.id} ,(err, resultat) => {
+ .findOneAndDelete( {'_id': ObjectID(req.params.id)} ,(err, resultat) => {
  if (err) return res.send(500, err)
  res.redirect('/adresses')
  })
 
 }) 
+
+//application pour tout détruire
+app.get("/vider", (req, res) => {
+	 db.collection('adresse').drop(req.body, (err, result) => { 
+ 	if (err) return console.log(err) 
+ 	console.log('sauvegarder dans la BD') 
+ res.redirect('/adresses') 
+})
+})
+
+//application modifier
+app.post('/modifier', (req, res) => {
+
+req.body._id = ObjectID(req.body._id)
+
+ db.collection('adresse').save(req.body, (err, result) => { 
+ 	if (err) return console.log(err) 
+ 	console.log('sauvegarder dans la BD') 
+ res.redirect('/adresses') 
+})
+})
 
 
 /*----------------------Connexion à MongoDB et au serveur Node.js-----------------------*/
